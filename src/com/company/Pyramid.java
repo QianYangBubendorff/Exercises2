aa14348841d5278f04f4b5682270bb9f0538e836
QianYangBubendorff <qian.yangbubendorff@gmail.com>
2022-03-09 15:28:10 +0100

    changes

--- src/com/company/Pyramid.java
+++ src/com/company/Pyramid.java
@@ -5,16 +5,16 @@ public class Pyramid {
         int n=5;
         for (int i = 1; i <=n; i++) {
             for (int j = i; j <= n; j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int j = 1; j<=i; j++){
-//                int intResult = (int) Math.pow(2, (j-1));
-                System.out.print((int) Math.pow(2, (j-1)));
+                int result = (int) Math.pow(2, (j-1));
+                System.out.print(result+" ");
                 }
-            for (int j = 1; j<i; j++){
-//                int intResult = (int) Math.pow(2, (n-i-j);
-                System.out.print((int) Math.pow(2, (n-i-j)));
-            }
+           for (int j = 1; j<i; j++){
+                int result = (int) Math.pow(2, (i-1-j));
+               System.out.print(result+" ");
+           }
 
                 System.out.println();
         }
