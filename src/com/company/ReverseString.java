2b0fa135768930818ede426a1b8ae71fff57026e
QianYangBubendorff <qian.yangbubendorff@gmail.com>
2022-03-09 20:14:22 +0100

    added Stringbuilder and Stringbuffer

---
+++ src/com/company/ReverseString.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.lang.*;
+
+    public class ReverseString {
+
+
+        public static void main(String args[]) {
+
+            //quick way to reverse String in Java - Use StringBuffer
+            StringBuffer buffer= new StringBuffer("HelloWorld");
+            buffer.reverse();
+            System.out.println(buffer);
+
+            StringBuilder str=new StringBuilder();
+            str.append("HelloWorld");
+            System.out.println(str.toString());
+
+                    //another quick to reverse String in Java - use StringBuilder
+            System.out.println(reverse("HelloWorld"));
+        }
+
+
+        public static String reverse(String source){
+            if(source == null || source.isEmpty()){
+                return source;
+            }
+            String reverse = "";
+            for(int i = source.length() -1; i>=0; i--){
+                reverse = reverse + source.charAt(i);
+            }
+
+            return reverse;
+        }
+
+    }
+
+
+
